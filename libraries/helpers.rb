f46870cc59b9a3f28c7d05ae935fc9f0672a6bbd
Alejandro Diaz <rubenalejandrodiaz@gmail.com>
2019-12-02 16:00:58 -0300

    Adding support for MySQL 8

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -111,6 +111,7 @@ module MysqlCookbook
       return ['mysql-client-5.5', 'libmysqlclient-dev'] if major_version == '5.5' && node['platform_family'] == 'debian'
       return ['mysql-client-5.6', 'libmysqlclient-dev'] if major_version == '5.6' && node['platform_family'] == 'debian'
       return ['mysql-client-5.7', 'libmysqlclient-dev'] if major_version == '5.7' && node['platform_family'] == 'debian'
+      return ['mysql-community-client', 'libmysqlclient-dev'] if major_version == '8.0' && node['platform_family'] == 'debian'
       return 'mysql-community-server-client' if major_version == '5.6' && node['platform_family'] == 'suse'
       %w(mysql-community-client mysql-community-devel)
     end
@@ -123,6 +124,7 @@ module MysqlCookbook
       return 'mysql-server-5.5' if major_version == '5.5' && node['platform_family'] == 'debian'
       return 'mysql-server-5.6' if major_version == '5.6' && node['platform_family'] == 'debian'
       return 'mysql-server-5.7' if major_version == '5.7' && node['platform_family'] == 'debian'
+      return 'mysql-community-server' if major_version == '8.0' && node['platform_family'] == 'debian'
       return 'mysql-community-server' if major_version == '5.6' && node['platform_family'] == 'suse'
       'mysql-community-server'
     end
